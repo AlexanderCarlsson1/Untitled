d29819e303757fb6b821f0b21f895d72440b9591
Alex <143814745+AlexanderCarlsson1@users.noreply.github.com>
2024-02-08 11:58:13 +0100

    Smooth player rotation

--- Biohazard/Assets/scripts/player/PlayerMovement.cs
+++ Biohazard/Assets/scripts/player/PlayerMovement.cs
@@ -15,5 +15,12 @@ public class PlayerMovement : MonoBehaviour
         moveInput.Normalize();
 
         rigidbody.velocity = moveInput * moveSpeed;
+
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rot = Quaternion.Euler(0f, 0f, angle);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, 10 * Time.deltaTime);
+
     }
 }
