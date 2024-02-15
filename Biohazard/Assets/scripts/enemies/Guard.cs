08d5a7115b20b0498ec52396f5b9278108d42fa7
Alex <143814745+AlexanderCarlsson1@users.noreply.github.com>
2024-02-15 10:38:19 +0100

    Guard player detection

--- Biohazard/Assets/scripts/enemies/Guard.cs
+++ Biohazard/Assets/scripts/enemies/Guard.cs
@@ -1,19 +1,55 @@
+using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject target;
+    public bool spotted = false;
+    public bool searching = false;
 
-    private static Transform currentTarget;
+    float searchTimer = 0;
 
     private void Update()
     {
-        if (currentTarget)
-        {
+        ManageSearch();
+    }
 
+    void ManageSearch()
+    {
+        if (searching == true && spotted == false)
+        {
+            searchTimer += Time.deltaTime;
+        }
+        if (searchTimer >= 6)
+        {
+            searching = false;
+        }
+        if (searching == true && spotted == false)
+        {
+            Debug.Log("searching");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AIDestinationSetter destionationSetter = GetComponent<AIDestinationSetter>();
+
+        if (collision.CompareTag("Player"))
+        {
+            destionationSetter.target = collision.transform;
+            searching = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Vector2 playerLastSeen = GameObject.FindWithTag(("Player")).GetComponent<Transform>().position;
+
+        if (GameObject.FindWithTag("Player"))
+        {
+            spotted = false;
+            GetComponent<AIDestinationSetter>().target = null;
         }
     }
 }
