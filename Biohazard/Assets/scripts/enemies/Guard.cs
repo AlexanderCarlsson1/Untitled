055ba18824ace45e7a7f9ecb31ee559c026fee75
Alex <143814745+AlexanderCarlsson1@users.noreply.github.com>
2024-02-15 09:30:54 +0100

    Added Guard and pathfinding

---
+++ Biohazard/Assets/scripts/enemies/Guard.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    private static Transform currentTarget;
+
+    private void Update()
+    {
+        if (currentTarget)
+        {
+
+        }
+    }
+}
